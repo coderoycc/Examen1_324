0672c4764f9ad3aaf96a42a387c6e020a140e517
coderoycc <90710352+coderoycc@users.noreply.github.com>
2021-10-03 20:00:58 -0400

    Add files via upload

---
+++ CaseWhen/mostrar.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="mostrar.aspx.cs" Inherits="WebApplication1.CaseWhen.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="notas" runat="server" AutoGenerateColumns="true">
+            
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ CaseWhen/mostrar.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace WebApplication1.CaseWhen
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "select avg(notafinal) as Media_Nota, case departamento when '02' then 'LA PAZ' when '03' then 'COCHABAMBA' when '04' then 'ORURO' end as Departamento from notas group by departamento;";
+                    cmd.Connection = conn;
+                    conn.Open();
+                    notas.DataSource = cmd.ExecuteReader();
+                    notas.DataBind();
+
+                }
+            }
+
+        }
+    }
+}
